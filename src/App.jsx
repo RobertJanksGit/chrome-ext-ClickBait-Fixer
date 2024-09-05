db47b25c61c1d4d7e7027d08adfb06c73b31fa09
Robert Jank <robert.jank@yahoo.com>
2024-09-05 09:18:17 -0500

    added in buttons and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [noMoreClickbait, setNoMoreClickbait] = useState(false);
+  const [evenMoreClickbait, setEvenMoreClickbait] = useState(false);
+  const [justForLaughs, setJustForLaughs] = useState(false);
+
+  const mainHeader = () => {
+    if (noMoreClickbait) return "No More Clickbait!";
+    if (evenMoreClickbait) return "Even More ClickBait!";
+    if (justForLaughs) return "Just Having Some Fun";
+    else return "Please Select An Option";
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="flex flex-col items-center space-y-4">
+        <img
+          src="../dist/images/no-more-clickbait-logo.png"
+          className="w-80"
+          alt="logo"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>{mainHeader()}</h1>
+      <div className="card flex space-x-4">
+        <button className="flex-1 text-black py-3 px-4 rounded">
+          No More Clickbait
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="flex-1 text-black py-3 px-4 rounded">
+          Even More Clickbait
+        </button>
+        <button className="flex-1 text-black py-3 px-4 rounded">
+          Just for laughs
+        </button>
       </div>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
