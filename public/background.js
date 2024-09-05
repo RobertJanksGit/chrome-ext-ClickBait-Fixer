a9fae6fc024e70acd5a5d53b4c8aea75f60921ee
Robert Jank <robert.jank@yahoo.com>
2024-09-05 13:51:06 -0500

    added option to click type of headline change

--- public/background.js
+++ public/background.js
@@ -1,6 +1,14 @@
 // Function to send a POST request to the server
+
 async function fetchLessClickbaityHeadline(headline) {
+  let type;
+  await chrome.storage.local.get(["type"], function (result) {
+    type = result.type;
+    console.log("Type currently is " + type);
+  });
+  console.log(type);
   try {
+    console.log(`Headline: ${headline}, the type is: ${type}`);
     const response = await fetch(
       "https://no-more-clickbait-project.onrender.com/",
       {
@@ -8,7 +16,45 @@ async function fetchLessClickbaityHeadline(headline) {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ headline: headline }),
+        body: JSON.stringify({ headline: headline, type: type }),
+        mode: "cors",
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.content;
+    } else {
+      throw new Error("Failed to fetch new headline: " + response.statusText);
+    }
+  } catch (error) {
+    console.error("Error fetching new headline:", error);
+    return "Error fetching new headline";
+  }
+}
+// Function to send a POST request to the server
+
+async function fetchLessClickbaityHeadline(headline) {
+  let type;
+  type = await new Promise((resolve) => {
+    chrome.storage.local.get(["type"], function (result) {
+      resolve(result.type);
+      console.log("Type currently is " + type);
+    });
+  });
+
+  console.log(type);
+
+  try {
+    console.log(`Headline: ${headline}, the type is: ${type}`);
+    const response = await fetch(
+      "https://no-more-clickbait-project.onrender.com/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ headline: headline, type: type }),
         mode: "cors",
       }
     );
@@ -34,6 +80,29 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       .catch((error) => {
         sendResponse({ newHeadline: "Error: Could not fetch new headline" });
       });
+
     return true; // Indicates that the response is sent asynchronously
   }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  chrome.storage.local.set({ type: request.selectedOption });
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action == "fetchHeadline") {
+    fetchLessClickbaityHeadline(request.headline)
+      .then((newHeadline) => {
+        sendResponse({ newHeadline: newHeadline });
+      })
+      .catch((error) => {
+        sendResponse({ newHeadline: "Error: Could not fetch new headline" });
+      });
+
+    return true; // Indicates that the response is sent asynchronously
+  }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  chrome.storage.local.set({ type: request.selectedOption });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,36 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [noMoreClickbait, setNoMoreClickbait] = useState(false);
-  const [evenMoreClickbait, setEvenMoreClickbait] = useState(false);
-  const [justForLaughs, setJustForLaughs] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(""); // Single state for managing selected option
 
   const mainHeader = () => {
-    if (noMoreClickbait) return "No More Clickbait!";
-    if (evenMoreClickbait) return "Even More ClickBait!";
-    if (justForLaughs) return "Just Having Some Fun";
-    else return "Please Select An Option";
+    switch (selectedOption) {
+      case "no-more-clickbait":
+        return "No More Clickbait!";
+      case "even-more-clickbait":
+        return "Even More ClickBait!";
+      case "just-for-laughs":
+        return "Just Having Some Fun";
+      default:
+        return "Please Select An Option";
+    }
+  };
+
+  const handleOnClick = (evt) => {
+    // evt.preventDefault();
+    const { name } = evt.target;
+    setSelectedOption(name);
+
+    //Send message to background script
+    chrome.runtime.sendMessage(
+      { action: "tacos", selectedOption: name },
+      (response) => {}
+    );
   };
 
   return (
     <>
-      <div class="flex flex-col items-center space-y-4">
+      <div className="flex flex-col items-center space-y-4">
         <img
           src="../dist/images/no-more-clickbait-logo.png"
           className="w-80"
@@ -24,13 +40,31 @@ function App() {
       </div>
       <h1>{mainHeader()}</h1>
       <div className="card flex space-x-4">
-        <button className="flex-1 text-black py-3 px-4 rounded">
+        <button
+          onClick={handleOnClick}
+          name="no-more-clickbait"
+          className={`flex-1 text-black py-3 px-4 rounded ${
+            selectedOption === "no-more-clickbait" ? "bg-gray-200" : ""
+          }`}
+        >
           No More Clickbait
         </button>
-        <button className="flex-1 text-black py-3 px-4 rounded">
+        <button
+          onClick={handleOnClick}
+          name="even-more-clickbait"
+          className={`flex-1 text-black py-3 px-4 rounded ${
+            selectedOption === "even-more-clickbait" ? "bg-gray-200" : ""
+          }`}
+        >
           Even More Clickbait
         </button>
-        <button className="flex-1 text-black py-3 px-4 rounded">
+        <button
+          onClick={handleOnClick}
+          name="just-for-laughs"
+          className={`flex-1 text-black py-3 px-4 rounded ${
+            selectedOption === "just-for-laughs" ? "bg-gray-200" : ""
+          }`}
+        >
           Just for laughs
         </button>
       </div>
